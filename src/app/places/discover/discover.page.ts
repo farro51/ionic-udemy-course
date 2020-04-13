0ee902473e15a74e40d88568c89eac046dc3047f
Federico Arroyave Ospina <farroyav@bancolombia.com.co>
2020-04-13 11:10:38 -0500

    Cambios modulo de estilos

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuController } from '@ionic/angular';
+import { SegmentChangeEventDetail } from '@ionic/core';
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
 
@@ -10,11 +10,17 @@ import { Place } from '../place.model';
 })
 export class DiscoverPage implements OnInit {
   loadedPlaces: Place[];
+  listedLoadedPlaces: Place[];
   constructor(private placesService: PlacesService//, private menuCtrl: MenuController
     ) { }
 
   ngOnInit() {
     this.loadedPlaces = this.placesService.places;
+    this.listedLoadedPlaces = this.loadedPlaces.splice(1);
+  }
+
+  onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
+    console.log(event.detail);
   }
 
   // onOpenMenu() {
--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { NavController, ModalController } from '@ionic/angular';
+import { NavController, ModalController, ActionSheetController } from '@ionic/angular';
 import { PlacesService } from '../../places.service';
 import { Place } from '../../place.model';
 import { CreateBookingComponent } from 'src/app/bookings/create-booking/create-booking.component';
@@ -14,6 +14,7 @@ export class PlaceDetailPage implements OnInit {
   place: Place;
   constructor(private navCtrl: NavController,
               private modalCtrl: ModalController,
+              private actionSheetController: ActionSheetController,
               private activatedRoute: ActivatedRoute,
               private placesService: PlacesService) { }
 
@@ -32,6 +33,27 @@ export class PlaceDetailPage implements OnInit {
     // Hace el efecto de q esta regresando a la pagina anterior y no creando una nueva
     //this.navCtrl.navigateBack('/places/tabs/discover'); Se devuelve con el efeto back
     //this.navCtrl.pop(); Solo cuando es garantizado q hay una página previa
+    this.actionSheetController.create({
+      header: 'Choose an Action',
+      buttons: [
+        {
+          text: 'Select Date',
+          handler: () => this.openBookingModal('select')
+        },
+        {
+          text: 'Random Date',
+          handler: () => this.openBookingModal('random')
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel' // 'destructive' pone el boton rojo
+        }
+      ]
+    }).then(actionSheetEl => actionSheetEl.present());
+  }
+
+  openBookingModal(mode: 'select' | 'random') {
+    console.log(mode);
     this.modalCtrl.create({
       component: CreateBookingComponent,
       componentProps: {
